2f2d74cff5f8af3f76e7878df7c4456d5ef51703
AzmiOzmn <azmiozmen5234@gmail.com>
2025-01-21 19:31:58 +0300

    update

--- Cental.BusinessLayer/Concrate/AboutManager.cs
+++ Cental.BusinessLayer/Concrate/AboutManager.cs
@@ -9,10 +9,39 @@ using System.Threading.Tasks;
 
 namespace Cental.BusinessLayer.Concrate
 {
-    public class AboutManager : GenericManager<About> , IAboutService
-    {
-        public AboutManager(IGenericDal<About> genericDal) : base(genericDal)
-        {
-        }
-    }
+	public class AboutManager : IAboutService
+	{
+		private readonly IAboutDal _abouDal;
+
+		public AboutManager(IAboutDal abouDal)
+		{
+			_abouDal = abouDal;
+		}
+
+		public void TCreate(About entity)                            
+		{
+			_abouDal.Create(entity);
+		}
+
+		public void TDelete(int id)
+		{
+			_abouDal.Delete(id);
+		}
+
+		public List<About> TGetAll()
+		{
+			return _abouDal.GetAll();
+		}
+
+		public About TGetById(int id)
+		{
+			return _abouDal.GetById(id);
+		}
+
+		public void TUpdate(About entity)
+		{
+			_abouDal.Update(entity);
+		}
+	}
+
 }
---
+++ Cental.WebUI/Mappings/AboutMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Cental.DtoLayer.AboutDtos;
+using Cental.EntityLayer.Entities;
+
+namespace Cental.WebUI.Mappings
+{
+    public class AboutMapping : Profile
+    {
+        public AboutMapping()
+        {
+            var thisYear = DateTime.Now.Year;
+            CreateMap<About , ResultListAboutDto>().ForMember(dst=>dst.ExperienceYear,o=>o.MapFrom(src=>thisYear-src.StartYear));
+            CreateMap<About , CreateAboutDto>().ReverseMap();
+            CreateMap<About , UpdateAboutDto>().ReverseMap();
+            CreateMap<About , ResultAboutDto>().ReverseMap();
+        }         
+    }
+}

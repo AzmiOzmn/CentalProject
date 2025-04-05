a6b64e867b5568cb21450ba433a5700925be6182
AzmiOzmn <azmiozmen5234@gmail.com>
2025-04-05 23:08:43 +0300

    update

---
+++ Cental.BusinessLayer/Concrete/IGenericManager.cs
@@ -0,0 +1,45 @@
+﻿using Cental.BusinessLayer.Abstract;
+using Cental.DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cental.BusinessLayer.Concrete
+{
+    public class IGenericManager<T> : IGenericService<T> where T : class
+    {
+        private readonly IGenericDal<T> _genericDal;
+
+        public IGenericManager(IGenericDal<T> genericDal)
+        {
+            _genericDal = genericDal;
+        }
+
+        public void TDelete(int id)
+        {
+          _genericDal.Delete(id);
+        }
+
+        public List<T> TGetAll()
+        {
+            return _genericDal.GetAll();
+        }
+
+        public T TGetById(int id)
+        {
+            return _genericDal.GetById(id);
+        }
+
+        public void TInsert(T entity)
+        {
+         _genericDal.Insert(entity);
+        }
+
+        public void TUpdate(T entity)
+        {
+          _genericDal.Update(entity);
+        }
+    }
+}
---
+++ Cental.WebUI/Controllers/AdminAboutController.cs
@@ -0,0 +1,92 @@
+﻿using Cental.BusinessLayer.Abstract;
+using Cental.DtoLayer.AboutDtos;
+using Cental.EntityLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cental.WebUI.Controllers
+{
+    public class AdminAboutController : Controller
+    {
+        private readonly IAboutService _aboutService;
+
+        public AdminAboutController(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _aboutService.TGetAll();
+            return View(values);
+        }
+
+        [HttpGet]
+
+        public IActionResult Insert()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Insert(InsertAboutDto model)
+        {
+            _aboutService.TInsert(new About
+            {
+                Vision = model.Vision,
+                Mision = model.Mision,
+                Description1 = model.Description1,
+                Description2 = model.Description2,
+                StartYear = model.StartYear,
+                ImageUrl1 = model.ImageUrl1,
+                ImageUrl2 = model.ImageUrl2,
+                Item1 = model.Item1,
+                Item2 = model.Item2,
+                Item3 = model.Item3,
+                Item4 = model.Item4,
+                NameSurname = model.NameSurname,
+                JobTitle = model.JobTitle,
+                ProfilePicture = model.ProfilePicture
+            }); // Manuel Mapping
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _aboutService.TDelete(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(int id)
+        {
+            var values = _aboutService.TGetById(id);
+            return View(values);
+        }
+
+        [HttpPost]
+
+        public IActionResult Update(UpdateAboutDto model)
+        {
+            var about = new About
+            {
+                AboutId = model.AboutId,
+                Vision = model.Vision,
+                Mision = model.Mision,
+                Description1 = model.Description1,
+                Description2 = model.Description2,
+                StartYear = model.StartYear,
+                ImageUrl1 = model.ImageUrl1,
+                ImageUrl2 = model.ImageUrl2,
+                Item1 = model.Item1,
+                Item2 = model.Item2,
+                Item3 = model.Item3,
+                Item4 = model.Item4,
+                NameSurname = model.NameSurname,
+                JobTitle = model.JobTitle,
+                ProfilePicture = model.ProfilePicture
+            };
+            _aboutService.TUpdate(about);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
